798be3d575515d9719489a0254a42fb493276d3d
samisep <sam.sepehri@gmx.de>
2022-08-21 18:27:39 +0200

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -38,16 +38,25 @@ const determineWinner = (userChoice, computerChoice) => {
             return 'The computer won!';
         } else {
             return 'You won!';
-        };
-        if (userChoice === 'scissors') {
-            if (computerChoice === 'rock') {
-                return 'The computer won!';
-            } else {
-                return 'You won!';
-            }
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'Computer WON'
+        } else {
+            return "You WON!"
         }
     }
 }
-console.log(determineWinner('paper', 'scissors')); // prints something like 'The computer won!'
-console.log(determineWinner('paper', 'paper')); // prints something like 'The game is a tie!'
-console.log(determineWinner('paper', 'rock')); // prints something like 'The user won!'
+
+
+const playGame = () => {
+    const userChoice = getUserChoice('scissors');
+    const computerChoice = getComputerChoice();
+    console.log('User threw: ' + userChoice);
+    console.log('Computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
